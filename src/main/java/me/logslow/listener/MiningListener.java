e20d2cfe6279c77d168c99008fc03c450228316b
Amytistshop <bannovsasa266@gmail.com>
2026-05-11 19:12:21 +0500

    Update MiningListener.java

--- src/main/java/me/logslow/listener/MiningListener.java
+++ src/main/java/me/logslow/listener/MiningListener.java
@@ -4,87 +4,65 @@ import me.logslow.LogSlowPlugin;
 import me.logslow.util.LogUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockDamageAbortEvent;
-import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 public class MiningListener implements Listener {
 
     private final LogSlowPlugin plugin;
 
-    private final Set<UUID> slowedPlayers = new HashSet<>();
-
     public MiningListener(LogSlowPlugin plugin) {
         this.plugin = plugin;
+
+        startTask();
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onBlockDamage(BlockDamageEvent event) {
+    private void startTask() {
 
-        Player player = event.getPlayer();
-        Material material = event.getBlock().getType();
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
 
-        boolean onlyLogs = plugin.getConfig().getBoolean("only_logs", true);
+            double multiplier = plugin.getConfig().getDouble("slow_multiplier", 2.0);
 
-        if (onlyLogs && !LogUtil.isLog(material)) {
-            removeFatigue(player);
-            return;
-        }
+            int amplifier = calculateAmplifier(multiplier);
 
-        double multiplier = plugin.getConfig().getDouble("slow_multiplier", 2.0);
+            for (Player player : Bukkit.getOnlinePlayers()) {
 
-        int amplifier = calculateAmplifier(multiplier);
+                Block target = player.getTargetBlockExact(5);
 
-        PotionEffect effect = new PotionEffect(
-                PotionEffectType.SLOW_DIGGING,
-                40,
-                amplifier,
-                false,
-                false,
-                false
-        );
+                if (target == null) {
+                    remove(player);
+                    continue;
+                }
 
-        player.addPotionEffect(effect, true);
+                Material material = target.getType();
 
-        slowedPlayers.add(player.getUniqueId());
+                boolean onlyLogs = plugin.getConfig().getBoolean("only_logs", true);
 
-        if (plugin.getConfig().getBoolean("verbose", false)) {
-            plugin.getLogger().info(player.getName() + " mining " + material.name());
-        }
+                if (onlyLogs && !LogUtil.isLog(material)) {
+                    remove(player);
+                    continue;
+                }
+
+                PotionEffect effect = new PotionEffect(
+                        PotionEffectType.SLOW_DIGGING,
+                        10,
+                        amplifier,
+                        false,
+                        false,
+                        false
+                );
+
+                player.addPotionEffect(effect, true);
+            }
+
+        }, 1L, 1L);
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onAbort(BlockDamageAbortEvent event) {
-        removeFatigue(event.getPlayer());
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onBreak(BlockBreakEvent event) {
-
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            removeFatigue(event.getPlayer());
-        }, 1L);
-    }
-
-    private void removeFatigue(Player player) {
-
-        if (!slowedPlayers.contains(player.getUniqueId())) {
-            return;
-        }
-
+    private void remove(Player player) {
         player.removePotionEffect(PotionEffectType.SLOW_DIGGING);
-
-        slowedPlayers.remove(player.getUniqueId());
     }
 
     private int calculateAmplifier(double multiplier) {
