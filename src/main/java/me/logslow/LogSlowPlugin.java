768abaeb3b704f58e59c94eebd4349a360f94ceb
Amytistshop <bannovsasa266@gmail.com>
2026-05-15 16:51:42 +0500

    Update LogSlowPlugin.java

--- src/main/java/me/logslow/LogSlowPlugin.java
+++ src/main/java/me/logslow/LogSlowPlugin.java
@@ -1,34 +1,21 @@
 package me.logslow;
 
-import me.logslow.listener.MiningPacketListener;
-import me.logslow.mining.MiningManager;
+import me.logslow.listener.BreakCooldownListener;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LogSlowPlugin extends JavaPlugin {
 
-    private static LogSlowPlugin instance;
-
-    private MiningManager miningManager;
-
-    public static LogSlowPlugin getInstance() {
-        return instance;
-    }
-
-    public MiningManager getMiningManager() {
-        return miningManager;
-    }
-
     @Override
     public void onEnable() {
 
-        instance = this;
-
         saveDefaultConfig();
 
-        miningManager = new MiningManager(this);
+        getServer().getPluginManager().registerEvents(
+                new BreakCooldownListener(this),
+                this
+        );
 
-        new MiningPacketListener(this);
-
-        getLogger().info("Advanced mining engine enabled.");
+        getLogger().info("LogSlow enabled.");
     }
 }
